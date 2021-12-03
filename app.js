3f23e30ddab9dc3fd7e3aa8d5f1ab991925bfaf5
amartya-rs <saha.amartya@outlook.com>
2021-12-03 13:04:08 +0530

    refactor: updated

--- app.js
+++ app.js
@@ -6,16 +6,25 @@ button.addEventListener("click", handler);
 
 //click handler
 function handler() {
-  var reversedDob = "";
-  var dobString = dob.value.replaceAll("-", "");
 
-  for (var j = dobString.length; j >= 0; j--) {
-    reversedDob = reversedDob + dobString.charAt(j);
+  if (dob.value !== "") {
+    var dobString = dob.value.replaceAll("-", "");
+
+    var reversedDob = "";
+    for (var j = dobString.length; j >= 0; j--) {
+      reversedDob = reversedDob + dobString.charAt(j);
+    }
+
+    console.log(dobString, reversedDob)
+    //check if paldindrome
+    if (dobString === reversedDob) {
+      outputh2.innerText = "Hurrah! Your birthday is palindrome in " + dob.value + " format";
+    } else {
+      outputh2.innerText = "Opps! Your birthday is not a palindrome";
+    }
+  } 
+  else {
+    alert(" Enter your date of birth");
   }
 
-  //check if paldindrome
-  if (dobString === reversedDob) {
-    outputh2.innerText = "Hurrah! Your birthday is palindrome";
-    console.log("1");
-  }
 }
