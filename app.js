6717988f8d89db805d08a1e2dd15cdbdbf17c98b
amartya-rs <saha.amartya@outlook.com>
2022-01-10 13:21:45 +0530

    refactor: update ES6+ syntax

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-var button = document.querySelector(".check-button");
-var dob = document.querySelector(".dob");
-var outputh2 = document.querySelector(".output");
-
-button.addEventListener("click", handler);
+const button = document.querySelector(".check-button");
+const dob = document.querySelector(".dob");
+const outputh2 = document.querySelector(".output");
 
 //click handler
-function handler() {
+let handler = () => {
 
   if (dob.value !== "") {
-    var dobString = dob.value.replaceAll("-", "");
+    let dobString = dob.value.replaceAll("-", "");
 
-    var reversedDob = "";
-    for (var j = dobString.length; j >= 0; j--) {
+    let reversedDob = "";
+    for (let j = dobString.length; j >= 0; j--) {
       reversedDob = reversedDob + dobString.charAt(j);
     }
 
     console.log(dobString, reversedDob)
     //check if paldindrome
     if (dobString === reversedDob) {
-      outputh2.innerText = "Hurrah! Your birthday is palindrome in " + dob.value + " format";
+      outputh2.innerText = `Hurrah! Your birthday is palindrome in ${dob.value} format`;
     } else {
-      outputh2.innerText = "Opps! Your birthday is not a palindrome";
+      outputh2.innerText = `Opps! Your birthday is not a palindrome`;
     }
   } 
   else {
@@ -28,3 +26,6 @@ function handler() {
   }
 
 }
+
+button.addEventListener("click", handler);
+
